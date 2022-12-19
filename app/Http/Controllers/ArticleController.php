38646b72b48cc186630a88874350460b8cb9c8c8
Behnoosh Moshtagh <behnoosh.moshtagh@gmail.com>
2022-12-19 18:14:17 +0330

    Paginate

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+
+    public function index()
+    {
+        $articles = Article::paginate();
+        return response()->json([
+            'data' => $articles,
+            'meta' => [
+                'count'=> $articles->count()
+            ],
+        ] , 200);
+    }
+
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        Article::create([
+            'user_id'=> '1',
+            'title'=> $request->title,
+            'description' => $request->description,
+            'image'=> $request->image
+        ]);
+        return response()->json([
+            'message' => 'Create is Done'
+        ],201);
+    }
+
+    public function show($id)
+    {
+        $article= Article::findOrFail($id);
+        return response()->json([
+            'data' => $article
+            ],
+            200
+        );
+    }
+
+    public function edit($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        $article= Article::findOrFail($id);
+        $article->update($request->all());
+        return response()->json([
+            'message' => 'Updated is Done'
+        ],
+            204
+        );
+    }
+
+    public function destroy($id)
+    {
+        $article= Article::findOrFail($id)->delete();
+        return response()->json([
+            'message' => 'deleted is Done'
+        ],
+            200
+        );
+    }
+
+
+}

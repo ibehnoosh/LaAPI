5044dad7864e8be2caef04784c267c288aac715c
Behnoosh Moshtagh <behnoosh.moshtagh@gmail.com>
2022-12-20 23:35:24 +0330

    remove jwt

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,16 @@
 <?php
 
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function (){
     Route::group(['prefix' => 'auth'], function ($router) {
-        Route::post('login', 'AuthController@login');
-        Route::post('logout', 'AuthController@logout');
-        Route::post('refresh', 'AuthController@refresh');
-        Route::post('me', 'AuthController@me');
+        Route::post('login',  [AuthController::class, 'login']);
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::post('refresh', [AuthController::class, 'refresh']);
+        Route::post('me', [AuthController::class, 'me']);
     });
     Route::resource('article', ArticleController::class);
 });
